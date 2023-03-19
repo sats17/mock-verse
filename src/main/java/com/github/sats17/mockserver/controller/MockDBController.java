7e919decf0cb8ead7fe8a9d0da380af41edb1947
Satish Kumbhar <Satish.kumbhar@publicissapient.com>
2023-03-19 15:49:08 +0530

    Added support for file based cache

--- src/main/java/com/github/sats17/mockserver/controller/MockDBController.java
+++ src/main/java/com/github/sats17/mockserver/controller/MockDBController.java
@@ -1,5 +1,8 @@
 package com.github.sats17.mockserver.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,6 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.sats17.mockserver.configuration.MongoConfiguration;
 import com.github.sats17.mockserver.utility.Utility;
 import com.mongodb.client.FindIterable;
@@ -27,47 +35,51 @@ import com.mongodb.client.model.ReplaceOptions;
 public class MockDBController {
 
 	@Autowired
+	ResourceLoader resourceLoader;
+
+	@Autowired
 	public MongoConfiguration mongoConfig;
-	
+
 	public static Map<Integer, Object> map = new HashMap<>();
+	
+	public static ObjectMapper objectMapper = new ObjectMapper();
 
 	public final static String dbName = "mockdb";
 	public final static String collectionName = "mockcollection";
 
-
 	@PostMapping("/api/db/insert")
-	public Object insertDataToDB(@RequestParam String apiPath, @RequestParam String apiMethod, @RequestBody String body) {
+	public Object insertDataToDB(@RequestParam String apiPath, @RequestParam String apiMethod,
+			@RequestBody String body) {
 		if (!Utility.isValidPath(apiPath)) {
 			return ResponseEntity.ok("Query parameter apiPath should starts with / as it represent API Path.");
 		}
 		if (!Utility.isValidAPIMethod(apiMethod)) {
 			return ResponseEntity.ok("Query parameter apiMethod is not valid.");
 		}
-		
+
 		Integer hashCode = Utility.generateHashCode(apiMethod, apiPath);
-		
+
 		MongoClient mongoClient = mongoConfig.mongoClient();
 		MongoDatabase database = mongoClient.getDatabase(dbName);
 		MongoCollection<Document> collection = database.getCollection(collectionName);
-		
+
 		Document bodyDocument = Document.parse(body);
-		
+
 		Document query = new Document("_id", hashCode);
-		Document document = new Document("_id", hashCode)
-		                        .append("data", bodyDocument);
-		
+		Document document = new Document("_id", hashCode).append("data", bodyDocument);
+
 		ReplaceOptions options = new ReplaceOptions().upsert(true);
 
 		// Execute the replace operation
 		collection.replaceOne(query, document, options);
-		
-		
+
 		FindIterable<Document> resp = collection.find();
 		return resp.first().get("data");
 	}
-	
+
 	@PostMapping("/api/map/insert")
-	public Object insertDataToMap(@RequestParam String apiPath, @RequestParam String apiMethod, @RequestBody String body) {
+	public Object insertDataToMap(@RequestParam String apiPath, @RequestParam String apiMethod,
+			@RequestBody String body) {
 		if (!Utility.isValidPath(apiPath)) {
 			return ResponseEntity.ok("Query parameter apiPath should starts with / as it represent API Path.");
 		}
@@ -78,9 +90,10 @@ public class MockDBController {
 		map.put(hashCode, body);
 		return map.get(hashCode);
 	}
-	
+
 	@PostMapping("/api/file/insert")
-	public Object insertDataToFile(@RequestParam String apiPath, @RequestParam String apiMethod, @RequestBody String body) {
+	public Object insertDataToFile(@RequestParam String apiPath, @RequestParam String apiMethod,
+			@RequestBody String body) {
 		if (!Utility.isValidPath(apiPath)) {
 			return ResponseEntity.ok("Query parameter apiPath should starts with / as it represent API Path.");
 		}
@@ -88,8 +101,14 @@ public class MockDBController {
 			return ResponseEntity.ok("Query parameter apiMethod is not valid.");
 		}
 		Integer hashCode = Utility.generateHashCode(apiMethod, apiPath);
-		map.put(hashCode, body);
-		return map.get(hashCode);
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		try {
+			objectMapper.writeValue(new File("src/main/resources/" + hashCode.toString() + ".json"), body);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "Inserted data to file";
 	}
 
 	@RequestMapping(value = "/**", produces = "application/json")
@@ -97,25 +116,32 @@ public class MockDBController {
 		String requestPath = request.getRequestURI();
 		String requestMethod = request.getMethod();
 		System.out.println("Path is " + requestPath);
-		System.out.println("Method is "+ requestMethod);
-		
+		System.out.println("Method is " + requestMethod);
+
 		Integer key = Utility.generateHashCode(requestMethod, requestPath);
-		
+
 		Object result = fetchDataFromDB(key);
-		if(result != null) {
-			System.out.println(((Document) result).get("data"));
+		if (result != null) {
+			System.out.println("Data returned from DB");
 			return ((Document) result).get("data");
 		}
-		
+
 		result = fetchDataFromMap(key);
 		if (result != null) {
-			System.out.println(result);
+			System.out.println("Data returned from Map");
 			return result;
 		}
+
+		result = fetchDataFromFile(key);
+		if (result != null) {
+			System.out.println("Data returned from FIle");
+			return result;
+		}
+		System.out.println("Data for key "+key+" not present anywhere in mockserver");
 		return "Data not found";
 
 	}
-	
+
 	private Object fetchDataFromDB(Integer key) {
 		MongoClient mongoClient = mongoConfig.mongoClient();
 		MongoDatabase database = mongoClient.getDatabase(dbName);
@@ -123,11 +149,25 @@ public class MockDBController {
 
 		Document query = new Document("_id", key);
 		Document result = collection.find(query).first();
-		return result;	
+		return result;
 	}
-	
+
 	private Object fetchDataFromMap(Integer key) {
 		return map.get(key);
+	}
+
+	private Object fetchDataFromFile(Integer key) {
+		try {
+			InputStream inputStream = new FileSystemResource("src/main/resources/" + key.toString() + ".json")
+					.getInputStream();
+
+			String output = objectMapper.readValue(inputStream, String.class);
+			System.out.println(output);
+			return output;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 }
